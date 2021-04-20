adf088e2d288e3a175d33941679c6045b6c38a9c
Donoras <donoras@gmail.com>
2021-04-20 08:26:31 +0300

    Smallfixes

--- models/res_partner_inherited.py
+++ models/res_partner_inherited.py
@@ -4,9 +4,5 @@ from odoo import fields, models
 class Partner(models.Model):
     _inherit = 'res.partner'
 
-    # Add a new column to the res.partner model, by default partners are not
-    # instructors
-    # instructor = fields.Boolean("Instructor", default=False)
-
     project_ids = fields.One2many('dn_projects.project', 'client_id',
         string="Projects", readonly=True)
