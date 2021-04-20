d8305642575b7c504646263ffaaa001dff7699a5
Donoras <donoras@gmail.com>
2021-04-20 11:06:16 +0300

    Added max_employees and emp_percent fields with progressbar to form and tree

--- models/project.py
+++ models/project.py
@@ -9,6 +9,7 @@ class Project(models.Model):
     description = fields.Text(string="Description")
     start_date = fields.Date(string="Start Date")
     end_date = fields.Date(string="End Date")
+    max_employees = fields.Integer(string="Max Employees")
 
     leader_id = fields.Many2one('hr.employee', string="Team Leader", domain=[('leader', '=', True)])
     client_id = fields.Many2one('res.partner', string="Client")
@@ -16,4 +17,12 @@ class Project(models.Model):
     jobs_ids = fields.One2many('dn_projects.job', 'project_id', string="Jobs")
     invoice_ids = fields.One2many('dn_projects.invoice', 'project_id', string="Invoices")
     employees_ids = fields.Many2many('hr.employee', string="Employees")
-    
+    emp_percent = fields.Float(string="Employee percent", compute='_get_employee_percent')
+
+    @api.depends('max_employees', 'employees_ids')
+    def _get_employee_percent(self):
+        for record in self:
+            if not record.max_employees:
+                record.emp_percent = 0.0
+            else:
+                record.emp_percent = 100.0 * len(record.employees_ids) / record.max_employees
