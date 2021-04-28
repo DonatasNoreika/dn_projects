2509ae7fb4a3d5f4ad3afd8ee14a8b8912a80e4a
Donoras <donoras@gmail.com>
2021-04-28 11:32:59 +0300

    8-1: added invoice statusbar

--- models/invoice.py
+++ models/invoice.py
@@ -7,5 +7,11 @@ class Invoice(models.Model):
 
     name = fields.Char(string="Title", required=True)
     total = fields.Float(string="Total")
+    status = fields.Selection([
+        ('draft', "Draft"),
+        ('sent', "Sent"),
+        ('paid', "Paid"),
+        ('cancelled', "Cancelled"),
+    ], string="Progress", default='draft', translate=True)
 
     project_id = fields.Many2one('dn_projects.project', string="Project")
